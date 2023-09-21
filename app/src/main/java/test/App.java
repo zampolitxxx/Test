fc6521de1a6e96b5474b20ad7b5a6d0f00b42ef0
zampolitxxx <zampolitxxx@gmail.com>
2023-09-21 16:54:52 +0900

    sldfk

--- app/src/main/java/test/App.java
+++ app/src/main/java/test/App.java
@@ -4,11 +4,8 @@
 package test;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        System.out.println(Sum.sum(1, 2));
     }
 }
---
+++ app/src/main/java/test/Sum.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Sum {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
